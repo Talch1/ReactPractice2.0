64b5278c05cb5b6da4d3e14a17a5a3388f8e5d7a
Talch1 <geylert@gmail.com>
2020-11-04 19:47:53 +0200

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-
-import './App.css';
+import "./App.css";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Todo from "./components/Todo";
 
 function App() {
   return (
     <div className="App">
-    
+      <div className="container">
+        <div className="row">
+          <div className="col-6">
+            <Register />
+          </div>{" "}
+          <div className="col-6">
+            <Login />
+          </div>
+          <div className="container">
+            <div className="row justify-content-center">
+              <div className="m-t-10">
+                <Todo />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,1 +1,57 @@
-import React from 'react'
+import React from "react";
+import { Formik, Form, Field } from "formik";
+import * as Yup from "yup";
+
+const SignupSchema = Yup.object().shape({
+  firstName: Yup.string()
+    .min(2, "Too Short!")
+    .max(50, "Too Long!")
+    .required("Required"),
+  lastName: Yup.string()
+    .min(2, "Too Short!")
+    .max(50, "Too Long!")
+    .required("Required"),
+  email: Yup.string().email("Invalid email").required("Required"),
+  password: Yup.string()
+    .min(8, "Too Short!")
+    .max(25, "Too Long!")
+    .required("Required")
+});
+
+export const Register = () => (
+  <div>
+    <h1>Register</h1>
+    <Formik
+      initialValues={{
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: "",
+      }}
+      validationSchema={SignupSchema}
+      onSubmit={(values) => {
+        console.log(values);
+      }}
+    >
+      {({ errors, touched }) => (
+        <Form>
+          <Field name="firstName" />
+          {errors.firstName && touched.firstName ? (
+            <div>{errors.firstName}</div>
+          ) : null}
+          <Field name="lastName" />
+          {errors.lastName && touched.lastName ? (
+            <div>{errors.lastName}</div>
+          ) : null}
+          <Field name="email" type="email" />
+          {errors.email && touched.email ? <div>{errors.email}</div> : null}
+           <Field name="password" type="password" />
+          {errors.password && touched.password ? <div>{errors.password}</div> : null}
+          <button type="submit">Submit</button>
+        </Form>
+      )}
+    </Formik>
+  </div>
+);
+
+export default Register;
